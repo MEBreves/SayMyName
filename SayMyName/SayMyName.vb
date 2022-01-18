d26f58f0b382a6961846cb85fe42dbb8e9c64a74
mbrev <mbrev@LAPTOP-2DN58FBI>
2022-01-18 08:52:58 -0700

    Add project files.

---
+++ SayMyName/SayMyName.vb
@@ -0,0 +1,34 @@
+﻿' Miranda Breves
+'RCET0265
+'Spring 2022
+'Say My Name
+'url
+
+Option Explicit On
+Option Strict On
+
+Module SayMyName
+
+    Sub Main()
+
+        'Variable Declaration
+        Dim name As String
+
+        Console.WriteLine("Hello, what's your name?")
+        name = Console.ReadLine()
+
+        'Creating a blank space to make the interaction pretty
+        Console.WriteLine()
+
+        Console.WriteLine("It's nice to meet you, " & CStr(name) & ".")
+
+        'Creating another blank space for formatting.
+        Console.WriteLine()
+
+        Console.WriteLine("Press any key to exit this program.")
+
+        Console.Read()
+
+    End Sub
+
+End Module
